73225271399160fe58b9bc3b43ed70bd3ccbe8e7
mdHassan <hassanqaisar01@gmail.com>
2024-03-14 16:27:08 +0530

    react project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+import Chai from "./chai";
+
+function App() {
+
+
+  return (
+    <div>
+      <Chai />
+      <h2>hello hassan</h2>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et doloribus corrupti soluta dolore dicta expedita officia laborum assumenda, repellat ipsum natus, officiis neque.</p>
+
+    </div>
+
+  );
+}
+
+export default App
---
+++ 06CurrencyConvertor/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className='text-3xl bg-orange-500'>Currency App</h1>
+    </div>
+      
+     
+      
+  )
+}
+
+export default App
